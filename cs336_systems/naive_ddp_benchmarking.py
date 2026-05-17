2503be91b9cbef7779605a480233871be1d74845
meichifan <chen_zhanyu@outlook.com>
2026-05-17 16:51:48 +0200

    Add naive DDP benchmarking script

---
+++ cs336_systems/naive_ddp_benchmarking.py
@@ -0,0 +1,63 @@
+import os
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import time
+from cs336_systems.transformer import Transformer
+
+def setup(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+    dist.init_process_group(backend="gloo", rank=rank, world_size=world_size)
+    # torch.cuda.set_device(rank)
+
+def cleanup():
+    dist.destroy_process_group()
+
+    
+def train(rank, world_size, num_steps=5):
+    setup(rank, world_size)
+
+    model = Transformer(d_model=2560,  d_ff=10240, num_layers=32, num_heads=32, vocab_size=1024)
+    model.load_state_dict(torch.load("initial_weights.pt", weights_only=True))
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    
+    step_times = []
+    reduce_times = []
+    for step in range(num_steps):
+        torch.manual_seed(step)
+        torch.cuda.synchronize()
+        t0 = time.time()
+        x = torch.randint(0, 1024, (4, 512))
+        shard_size = x.shape[0] // world_size
+        x_shard = x[rank*shard_size: (rank+1)*shard_size]
+        optimizer.zero_grad()
+        output = model(x_shard, 512, 10000.0)
+        loss = output.sum()
+        loss.backward()
+    
+        
+        torch.cuda.synchronize()
+        t1= time.time()
+        for param in model.parameters():
+            dist.all_reduce(param.grad, op=dist.ReduceOp.SUM)
+            param.grad /= world_size
+        torch.cuda.synchronize()
+        reduce_time = time.time() - t1
+        reduce_times.append(reduce_time)
+        optimizer.step()
+        torch.cuda.synchronize()
+        step_time = time.time() - t0
+        step_times.append(step_time)
+    
+    print(f"Rank {rank}")
+    print(f"Time spent: {sum(step_times) / len(step_times)}")
+    print(f"Reduce time accounts for {(sum(reduce_times) / len(reduce_times)) / (sum(step_times) / len(step_times))}")
+    cleanup()
+
+
+if __name__ == "__main__":
+    world_size = 2
+    model = Transformer(d_model=2560, d_ff=10240, num_layers=32, num_heads=32, vocab_size=1024)
+    torch.save(model.state_dict(), "initial_weights.pt")
+    mp.spawn(train, args=(world_size,), nprocs=world_size, join=True)
